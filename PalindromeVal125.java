6cdcb61c568ac06cd77100907fbdf49f247a0de3
roopa-codes <roopavenkatampalli8@gmail.com>
2025-08-16 12:33:24 +0530

    125 Valid Palindrome

---
+++ PalindromeVal125.java
@@ -0,0 +1,23 @@
+public class PalindromeVal125 {
+        public boolean isPalindrome(String s) {
+            int left = 0, right = s.length() - 1;
+            while (left < right) {
+                // traverse to next Alphanumeric character
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                    left++;
+                }
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                    right--;
+                }
+                // Compare ignoring case
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+}
